705162ee084b668e3d33129023ffe35e78771b35
Fuad2004 <fuadalfiroj2004@gmail.com>
2024-01-17 20:20:43 +0600

    Final Build

---
+++ Translate.py
@@ -0,0 +1,9 @@
+from googletrans import Translator
+
+def TranslateIt(sText, lang):
+    print(lang)
+    translator = Translator()
+
+    out = translator.translate(sText, dest=lang)
+
+    return out.text
